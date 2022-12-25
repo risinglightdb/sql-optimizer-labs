869ff61f6ef0f8ad6ce86d10be393aa6f14a9b45
Runji Wang <wangrunji0408@163.com>
2022-12-25 16:01:31 +0800

    lab1: language
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use egg::{define_language, Id, Symbol};
+
+mod value;
+
+pub use value::*;
+
+pub type RecExpr = egg::RecExpr<Expr>;
+
+define_language! {
+    pub enum Expr {
+        // values
+        Constant(Value),            // null, true, 1, 'hello'
+        Column(Symbol),             // t.a, b, c
+
+        // utilities
+        "list" = List(Box<[Id]>),   // (list ...)
+
+        // unary operations
+        "-" = Neg(Id),
+        "not" = Not(Id),
+        "isnull" = IsNull(Id),
+
+        // binary operations
+        "+" = Add([Id; 2]),
+        "-" = Sub([Id; 2]),
+        "*" = Mul([Id; 2]),
+        "/" = Div([Id; 2]),
+        "=" = Eq([Id; 2]),
+        "<>" = NotEq([Id; 2]),
+        ">" = Gt([Id; 2]),
+        "<" = Lt([Id; 2]),
+        ">=" = GtEq([Id; 2]),
+        "<=" = LtEq([Id; 2]),
+        "and" = And([Id; 2]),
+        "or" = Or([Id; 2]),
+        "xor" = Xor([Id; 2]),
+    }
+}
---
+++ tests/1_language.rs
@@ -0,0 +1,53 @@
+use egg_sql_labs::{Expr, RecExpr, Value};
+
+#[test]
+fn values() {
+    assert_parse_value("null", Value::Null);
+    assert_parse_value("true", Value::Bool(true));
+    assert_parse_value("1", Value::Int(1));
+    assert_parse_value("'string'", Value::String("string".into()));
+}
+
+#[test]
+fn columns() {
+    assert_parse_expr("a");
+    assert_parse_expr("t.a");
+}
+
+#[test]
+fn list() {
+    assert_parse_expr("(list null 1 2)");
+}
+
+#[test]
+fn operations() {
+    assert_parse_expr("(isnull null)");
+    assert_parse_expr("(- a)");
+    assert_parse_expr("(+ a b)");
+    assert_parse_expr("(- a b)");
+    assert_parse_expr("(* a b)");
+    assert_parse_expr("(/ a b)");
+    assert_parse_expr("(= a b)");
+    assert_parse_expr("(<> a b)");
+    assert_parse_expr("(> a b)");
+    assert_parse_expr("(< a b)");
+    assert_parse_expr("(>= a b)");
+    assert_parse_expr("(<= a b)");
+    assert_parse_expr("(not a)");
+    assert_parse_expr("(and a b)");
+    assert_parse_expr("(or a b)");
+    assert_parse_expr("(xor a b)");
+}
+
+#[track_caller]
+fn assert_parse_value(expr: &str, value: Value) {
+    assert_eq!(
+        expr.parse::<RecExpr>().unwrap()[0.into()],
+        Expr::Constant(value)
+    );
+}
+
+#[track_caller]
+fn assert_parse_expr(expr: &str) {
+    assert_eq!(expr.parse::<RecExpr>().unwrap().to_string(), expr);
+}
